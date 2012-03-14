24f7c66087fdda01f594bbb672f8d5e7e6967f66
Niranda <niranda@niranda.net>
2012-03-14 12:19:56 +0100

    changed NiraProxy Server to new address

--- lib/siriproxy-niraproxy.rb
+++ lib/siriproxy-niraproxy.rb
@@ -12,6 +12,8 @@ class SiriProxy::Plugin::NiraProxy < SiriProxy::Plugin
 		 #-------------------------------------------------------------------------------->
 		 #-> Additional configurations
 		 #-<
+		 
+		 @config = config															# Load config-parameters
 	end
 
 
@@ -21,19 +23,39 @@ class SiriProxy::Plugin::NiraProxy < SiriProxy::Plugin
  		 #-> Feel free to register on my great Niranda.net (http://www.Niranda.net) to get
  		 #-> special configurations for our API-Access. It's free.
  		 #-> Visit http://proxy.niranda.net for more information.
+ 		 #->
+ 		 ##-> Attention
+ 		 #-> Do not change the following lines. This are requirements for the NiraProxy.
  		 #-< 
  		 
+ 		np_version = "0.1.0"														# compatible NiraProxy Web-Version
+ 		answer = ""																	# default answer
+ 		success = false																# default answer-status (ask Apple...)
+ 		
+ 		
  		##-> Parameter-Usage
  		 #-------------------------------------------------------------------------------->
- 		 #-> id		= your Identity
- 		 #-> key	= your personal (preshared) key
+ 		 #-> If you like to change something, use the config-file (/.SiriProxy/config.yml)
+ 		 #->
+ 		 ##-> Parameter-Description 
+ 		 #-> id				your Identity (should be escaped)
+ 		 #-> key			your personal key (should be escaped)
+ 		 #-> inquiry		spoken inquiry (should be escaped)
  		 #-< 
  		 
- 		uri = "http://proxy.niranda.net/siri.php?id=default&key=default&inquiry="	# URI to NiraProxy-API
+ 		uri_server = "http://proxy.niranda.net"										# Server
+ 		uri_script = "/siri/siri.php"												# Path to script
+ 		uri_id = URLEscape.escape(@config['user_id'])								# User-ID
+ 		uri_key = URLEscape.escape(@config['user_secret'])							# User-Secret
+ 		uri_inquiry = URLEscape.escape(text)										# Innquiry
  		
+ 		uri = uri_server + uri_script + "?v=" + np_version + "&id=" + uri_id + "&key=" + uri_key + "&inquiry=" + uri_inquiry
+
  		
- 		uri << URLEscape.escape(text)												# Add HTMLized inquiry
- 		answer = ""																	# define default answer
+ 		##-> Send the inquiry and catch the answer
+ 		 #-------------------------------------------------------------------------------->
+ 		 #-> Just read out the given URI
+ 		 #-<
  		
  		open (uri) {																# execute the inquiry
 			|f|
@@ -43,20 +65,85 @@ class SiriProxy::Plugin::NiraProxy < SiriProxy::Plugin
  		
  		##-> Answer-Check
  		 #-------------------------------------------------------------------------------->
- 		 #-> If "%noanswer%" is given back, SiriProxy will ask the Apple-Servers
+ 		 #-> Processing the answer and choose the type of return
+ 		 #->
+ 		 ##-> Parameter-Description
+ 		 #-> %noanswer%		no answer, ask apple
+ 		 #-> %nirasplit%	splitter for seperat array-elements
+ 		 #-> %type-say%		say sth.
+ 		 #-> %type-ask%		ask sth.
+ 		 #-> %type-show%	show sth. (Wolfram Alpha Frame)
+ 		 #->
+ 		 ##-> Fallback
+ 		 #-> %update%		no answer, ask apple and remind to update
  		 #-<
  		 
- 		if (answer =~ /%noanswer%/i)
-			success = false														# well, ask Apple...
- 		else
-			success = true															# don't ask Apple!
+ 		# Nachricht splitten
+ 		answer_parts = answer.split('%nirasplit%')
+ 		answer_type = answer_parts.shift											# select answer-type, rest is the answer itself
+ 		 
+ 		if (answer_type =~ /%noanswer%/i)											# well, ask Apple...
+			success = false
+		
+		elsif (answer_type =~ /%update%/)											# Plugin-Version is to old, update
+			if (@config["remind_on_update"])
+				answer_parts.each do |textline|
+					say textline
+				end
+				
+				success = true
+				request_completed
+			
+			else			
+				success = false
+			end
+														
+ 		elsif (answer_type =~ /%type-ask%/i)										# question with options
+			# this functionallity is still in dev
+			success = false
+ 		
+ 		elsif (answer_type =~ /%type-show%/i)										# sth to show
+ 			answer_headline = answer_parts.shift
+ 			
+			object = SiriAddViews.new
+    		
+    		answer_show = SiriAnswer.new(answer_headline, [
+    			answer_parts.each do |textline|
+					SiriAnswerLine.new(textline)
+				end
+    		])
+    		
+    		object.views << SiriAnswerSnippet.new([answer_show])
+    		send_object object
+			
+			success = true
+			request_completed
+			
+		elsif (answer_type =~ /%type-say%/i)										# say it
+			answer_parts.each do |textline|
+				say textline
+			end
+			
+			success = true
+			request_completed
+			
+		
+		else																		# fallback, just say it...
+			answer_parts.each do |textline|
+				say textline
+			end
+			
+			success = true
+			request_completed											
+ 		
  		end
  		
- 		if (success)																# on success...
-			say answer																# ansering...
-			request_completed														# finish this cool script...
- 		end
  		
- 		return success																# success or no success? Return it! 
+ 		#if (success)																# on success...
+		#	say answer																# ansering...
+		#	request_completed														# finish this cool script...
+ 		#end
+ 		
+ 		return success																# success or not? Return it! 
 	end
 end
